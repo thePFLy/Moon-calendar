e09f4154e3c2debfcae27eba066dff0508161eb0
maxime <mux99@live.be>
2023-11-30 16:35:10 +0100

    FIX
    -remove histEqualization
    - added 1% increment circle size
    - display image when no circle is found
    - resize image bigger than 2000pixels
    - display multiple pictures at once
    - rework files

--- project_elec.py
+++ project_elec.py
@@ -1,15 +1,24 @@
 import numpy as np
+from tkinter import *
 from tkinter import Tk
 from tkinter.filedialog import askopenfilenames
 import cv2
 
-def sort_array(arr):
-    result = []
-    for i in range(3, len(arr) - 3):
-        if all(arr[i] > x for x in arr[i-3:i]) and all(arr[i] > x for x in arr[i+1:i+4]):
-            result.append(i)
-    result.sort(key=lambda x: arr[x] - ((sum(arr[x-3:x]) + sum(arr[x+1:x+4])) / 6), reverse=True)
-    return result
+def preprocessing(img):
+    #pre pocessing
+    greyscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(greyscale, (7, 7), 0)
+    size = min(img.shape[0],img.shape[1]) - 1 + (min(img.shape[0],img.shape[1])%2)
+    binary = cv2.adaptiveThreshold(blurred, 255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,size,0)
+
+    #clean artefacts
+    kernel1 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
+    kernel2 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(int(size*0.01),int(size*0.01)))
+    for i in range(5):
+        binary = cv2.morphologyEx(binary, cv2.MORPH_OPEN, kernel1)
+        binary = cv2.morphologyEx(binary, cv2.MORPH_CLOSE, kernel1)
+
+    return binary
 
 def sobel(img):
     size = min(img.shape[0],img.shape[1]) - 1 + (min(img.shape[0],img.shape[1])%2)
@@ -18,8 +27,7 @@ def sobel(img):
     abs_grad_x = cv2.convertScaleAbs(grad_x)
     abs_grad_y = cv2.convertScaleAbs(grad_y)
     wheighted = cv2.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0)
-    binary = cv2.adaptiveThreshold(wheighted, 255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,size//2,0)
-    binary = cv2.equalizeHist(binary)
+    binary = cv2.adaptiveThreshold(wheighted, 255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,size,0)
     return binary
 
 def find_circle(img):
@@ -34,7 +42,7 @@ def find_circle(img):
 
     #correlation convolutions
     candidates = []
-    for i in range(circle_max,circle_min,-10):
+    for i in range(circle_max,circle_min,-max(10,int((circle_max-circle_min)*0.01))):
         r = i//2
         #generate circle
         circle = np.zeros((i, i), dtype=np.uint8)
@@ -67,6 +75,14 @@ def find_circle(img):
 
     return candidates[peaks]
 
+def sort_array(arr):
+    result = []
+    for i in range(3, len(arr) - 3):
+        if all(arr[i] > x for x in arr[i-3:i]) and all(arr[i] > x for x in arr[i+1:i+4]):
+            result.append(i)
+    result.sort(key=lambda x: arr[x] - ((sum(arr[x-3:x]) + sum(arr[x+1:x+4])) / 6), reverse=True)
+    return result
+
 def get_luminosity(circle, img):
     global rect
     #crop to keep moon
@@ -91,11 +107,10 @@ def get_luminosity(circle, img):
     return left, right
 
 def get_lunar_day(left, right):
-    lum = 1-((left+right)/2)
-    if left > right:
-        day = (14.75*lum)+14.75
+    if right > left:
+        day = (14.75*((left+right)/2))+14.75
     else:
-        day = 14.75*lum
+        day = 14.75*(1-((left+right)/2))
     return day
 
 def superpose(img1, img2):
@@ -103,48 +118,36 @@ def superpose(img1, img2):
         for j in range(len(img1[0])):
             img2[i][j] = img1[i][j]
 
-def preprocessing(img):
-    #pre pocessing
-    greyscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    blurred = cv2.GaussianBlur(greyscale, (7, 7), 0)
+def output_img(img, graph, circle=None, day=None, left=None, right=None):
     size = min(img.shape[0],img.shape[1]) - 1 + (min(img.shape[0],img.shape[1])%2)
-    binary = cv2.adaptiveThreshold(blurred, 255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,size,0)
-
-    #clean artefacts
-    kernel1 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
-    kernel2 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(int(size*0.01),int(size*0.01)))
-    for i in range(5):
-        binary = cv2.morphologyEx(binary, cv2.MORPH_OPEN, kernel1)
-        binary = cv2.morphologyEx(binary, cv2.MORPH_CLOSE, kernel1)
-
-    return binary
-
-def output_img(img, circle, graph, day, left, right):
-    size = min(img.shape[0],img.shape[1]) - 1 + (min(img.shape[0],img.shape[1])%2)
-    tmp_rect = cv2.resize(rect, (int(size*0.25), int(size*0.25))) 
-    superpose(tmp_rect, img)
     
-    cv2.circle(img, (circle[0],circle[1]), circle[2], (255, 255, 0), 1)
-    cv2.circle(img, (circle[0],circle[1]), 2, (255, 255, 0), -1)
-    text_size = int(size*0.07)
+    if circle is not None:
+        cv2.circle(img, (circle[0],circle[1]), circle[2], (255, 255, 0), 1)
+        cv2.circle(img, (circle[0],circle[1]), 2, (255, 255, 0), -1)
+        text_size = int(size*0.07)
 
-    cv2.line(img, (0,0), (0, len(tmp_rect)), (255, 0, 255), 1)
-    cv2.line(img, (len(tmp_rect)//2,0), (len(tmp_rect)//2,len(tmp_rect)), (255, 0, 255), 1)
-    cv2.line(img, (len(tmp_rect),0), (len(tmp_rect),len(tmp_rect)), (255, 0, 255), 1)
-    cv2.line(img, (0,0), (len(tmp_rect), 0), (255, 0, 255), 1)
-    cv2.line(img, (0, len(tmp_rect)), (len(tmp_rect), len(tmp_rect)), (255, 0, 255), 1)
-    cv2.putText(img, f'Day: {int(day)} L: {round(left*100,2)}% R: {round(right*100,2)}%', (5, int(size*0.25)+(text_size//2)), cv2.FONT_HERSHEY_SIMPLEX, text_size/100, (255, 0, 255), 1, cv2.LINE_AA)
+    if (day is not None) or (day is not None) or (day is not None):
+        tmp_rect = cv2.resize(rect, (int(size*0.25), int(size*0.25))) 
+        superpose(tmp_rect, img)
+        cv2.line(img, (0,0), (0, len(tmp_rect)), (255, 0, 255), 1)
+        cv2.line(img, (len(tmp_rect)//2,0), (len(tmp_rect)//2,len(tmp_rect)), (255, 0, 255), 1)
+        cv2.line(img, (len(tmp_rect),0), (len(tmp_rect),len(tmp_rect)), (255, 0, 255), 1)
+        cv2.line(img, (0,0), (len(tmp_rect), 0), (255, 0, 255), 1)
+        cv2.line(img, (0, len(tmp_rect)), (len(tmp_rect), len(tmp_rect)), (255, 0, 255), 1)
+        cv2.putText(img, f'Day: {int(day)} L: {round(left*100,2)}% R: {round(right*100,2)}%', (5, int(size*0.25)+(text_size//2)), cv2.FONT_HERSHEY_SIMPLEX, text_size/100, (255, 0, 255), 1, cv2.LINE_AA)
 
     #add luminosity graph (slow things down ALOT)
     graph = np.array(graph)
     max_lum = max(graph[:,3])
     for i in range(len(graph)):
         color = (255, 0, 255)
-        if np.array_equal(graph[i],data[0]):
+        if circle is not None and np.array_equal(graph[i],data[0]):
             color = (255, 255, 0)
         val = int(len(img) - ((graph[i][3]/max_lum)*(len(img)/4)))
         cv2.line(img, (i*2,len(img)), (i*2,val), color, 1)
     return img
+
+# =====================================/ MAIN /==================================================================
 
 if __name__ == "__main__":
     global circles
@@ -156,12 +159,19 @@ if __name__ == "__main__":
 
     for f in files:
         initial = cv2.imread(f)
+        size = max(len(initial),len(initial[0]))
+        if (size > 2000):
+            scale = 1-((size-2000)/size)
+            initial = cv2.resize(initial, (int(len(initial[0])*scale), int(len(initial)*scale)))
         binary = preprocessing(initial)
         data = find_circle(binary)
-        left, right = get_luminosity(data[0], binary)
-        day = get_lunar_day(left, right)
-        output = output_img(initial, data[0], circles, day, left, right)
-        cv2.namedWindow("out", cv2.WINDOW_NORMAL)
-        cv2.imshow("out", output)
+        if len(data) > 0:
+            left, right = get_luminosity(data[0], binary)
+            day = get_lunar_day(left, right)
+            output = output_img(initial, circles, data[0], day, left, right)
+        else:
+            output = output_img(initial, circles)
+        cv2.namedWindow(f, cv2.WINDOW_NORMAL)
+        cv2.imshow(f, output)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
